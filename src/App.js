2a2a04a543bdf72df9f3e7a9184d64023115e515
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-08-12 19:49:59 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             0: "https://www.clipartmax.com/png/middle/72-729242_small-star-shape-stars-starred-favourite-outline-star-icon-svg.png",
             1: "https://www.clipartmax.com/png/middle/416-4166823_icon-constellation-comments-silver-star-background.png",
           }}
-          how={10} /* Pass the number of images Sky will render chosing randomly */
+          how={100} /* Pass the number of images Sky will render chosing randomly */
           time={40} /* time of animation */
           size={`${size}px`} /* size of the rendered images */
         />
